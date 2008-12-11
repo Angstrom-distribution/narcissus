3cd9f3eca312b9cb5c22f5319bdc67339d08eb6c
root <root@dominion.thruhere.net>
2008-12-11 18:48:14 +0100

    narcissus: initial checkin

---
+++ index.php
@@ -0,0 +1,34 @@
+<?
+/* Narcissus - Online image builder for the angstrom distribution
+ * Koen Kooi (c) 2008 - all rights reserved 
+ *
+ * basic operation:
+ * 1) select machine and assemble arch.conf
+ * 2) select package set
+ * 3) prime rootfs with opkg-collateral and angstrom-feed-configs
+ * 4) install package set
+ * 5) tar it up
+ */
+
+
+$base_pkg_set = " task-base angstrom-version ";
+
+function pkg_selection() {
+	print "foo!";
+}
+
+function assemble_image($pkgs) {
+	print "<pre>";
+	print "<b>bin/opkg-cl -o foo -f foo/etc/opkg.conf update</b><p>";
+	system("bin/opkg-cl -o foo -f foo/etc/opkg.conf update");
+	print "\n<b>bin/opkg-cl -o foo -f foo/etc/opkg.conf install $pkgs</b><p>";
+	system("bin/opkg-cl -o foo -f foo/etc/opkg.conf install $pkgs");
+	print "\n<b>tar cjf ../deploy/foo.tar.bz2</b><p>";
+	system ("cd foo ;tar cjf ../deploy/foo.tar.bz2 .; cd ..");
+	print "</pre>";
+	print "<a href='deploy/foo.tar.bz2'>your image</b>";
+}
+
+assemble_image($base_pkg_set);
+
+?>
