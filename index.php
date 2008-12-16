aad79f836b741514d520d4273f4dd5017bad56db
Koen Kooi <koen@dominion.thruhere.net>
2008-12-16 13:03:35 +0100

    narcissus: more packages, improve UI

--- index.php
+++ index.php
@@ -26,24 +26,42 @@ if ($handle = opendir ('./conf/'))
 }
 
 ?>
-<div id="form" class="kader"><form name="entry_form" onsubmit="javascript:configureImage();return false">
-  Machine:
 
-<select name="machine">
+This is a proof of concept online image builder for the Angstrom distribution. The basic operation is simple:
+<ol>
+<li>select machine from dropdown list</li>
+<li>check packages you want to have included</li>
+<li>enter the preferred name for the image that will be built</li>
+<li>press enter</li>
+</ol>
+
+<div id="form" class="kader"><form name="entry_form" onsubmit="javascript:configureImage();return false">
+Machine: <select name="machine">
 <? machine_dropdown(); ?>
 </select >
 
-  Image name:
-  <input type="text" name="name" id="name" />
 <hr width="80%"/>
 Package selections:<br/><br/>
 <?
-$package_array = array("task-base" => "task-base", "X11" => "angstrom-x11-base-depends", "Enlightenment" => "e-wm e-wm-config-standard e-wm-config-default" );
+$package_array = array("task-base" => "task-base", 
+                       "X11" => "angstrom-x11-base-depends angstrom-gpe-task-base", 
+                       "Enlightenment" => "e-wm e-wm-config-standard e-wm-config-default angstrom-x11-base-depends angstrom-gpe-task-base",
+                       "Metacity" => "metacity angstrom-x11-base-depends angstrom-gpe-task-base",
+                       "Samba" => "samba",
+                       "Apache" => "apache2",
+                       "Python" => "python-core python-modules",
+                       "Perl" => "perl perl-modules",
+                       "Toolchain" => "task-native-sdk",
+                       "Busybox replacements" => "task-proper-tools", 
+                       "Mplayer" => "mplayer");
 
 foreach ($package_array as $pkg => $pkgdepends) {
 	print("<input type=\"checkbox\" name=\"pkg\" value=\"$pkgdepends\">$pkg<br/>\n");
 }
 ?>
+<hr width="80%"/>
+ Image name:
+  <input type="text" name="name" id="name" />
 </form></div>
 
 <br clear='all'/>
