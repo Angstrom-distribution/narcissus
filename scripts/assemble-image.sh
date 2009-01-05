fa97ff436c8684dba03a9a60056e1b8654ff52a6
Koen Kooi <koen@dominion.thruhere.net>
2009-01-05 16:15:00 +0100

    narcissus: run over post- and preinst scripts and update opkg db accordingly

--- scripts/assemble-image.sh
+++ scripts/assemble-image.sh
@@ -9,6 +9,9 @@ WORKDIR="${PWD}"
 
 TARGET_DIR="${WORKDIR}/deploy/${MACHINE}/${IMAGENAME}"
 OPKG_CONFDIR_TARGET="${TARGET_DIR}/etc/opkg"
+export D="${TARGET_DIR}"
+export OPKG_OFFLINE_ROOT="${TARGET_DIR}"
+export PATH=${WORKDIR}/bin:${PATH}
 
 if ! [ -e ${TARGET_DIR}/etc/opkg.conf ] ; then
 	print "Initial filesystem not found, something went wrong in the configure step!"
@@ -22,6 +25,26 @@ bin/makedevs -r ${TARGET_DIR} -D conf/devtable.txt
 if [ -e ${TARGET_DIR}/log.txt ] ; then
 	rm ${TARGET_DIR}/log.txt
 fi
+
+echo "Running preinsts"
+
+for i in ${TARGET_DIR}/usr/lib/opkg/info/*.preinst; do
+	if [ -f $i ] && ! sh $i; then
+		echo "Running: opkg-cl -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf flag unpacked `basename $i .preinst`"
+		opkg-cl -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf flag unpacked `basename $i .preinst`
+	fi
+done 
+
+echo "Running postinstinsts"
+
+for i in ${TARGET_DIR}/usr/lib/opkg/info/*.postinst; do
+	if [ -f $i ] && ! sh $i configure; then
+		echo "Running: opkg-cl -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf flag unpacked `basename $i .postinst`"
+		opkg-cl -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf flag unpacked `basename $i .postinst`
+	fi
+done 
+
+ls ${TARGET_DIR}/sbin -la
 
 echo "removing opkg index files"
 rm ${TARGET_DIR}/var/lib/opkg/* || true
