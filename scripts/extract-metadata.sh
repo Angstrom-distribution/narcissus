4f483fb34366172a1a23fc05c004f4bc51ba2fcf
Koen Kooi <koen@dominion.thruhere.net>
2010-03-15 12:27:40 +0100

    narcissus: add script to extract metadata from the cachedir with dpkg-deb

---
+++ scripts/extract-metadata.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Narcissus - Online image builder for the angstrom distribution
+# Koen Kooi (c) 2010- all rights reserved 
+
+# This script extracts metadata from the cache dir to use in e.g. manifests
+
+if [ -e ${PWD}/conf/host-config ] ; then
+    . ${PWD}/conf/host-config
+    echo "Loaded host config"
+fi
+
+if [ -e ${CACHEDIR} ] ; then
+	( cd ${CACHEDIR}
+	rm -f  metadata.txt
+	for i in *ipk ; do
+		dpkg-deb -I $i > control
+		LICENSE="$(grep License control | awk -F": " '{print $2}')"
+		FILENAME="$(echo $i |  awk -F, '{print $NF}')"
+		echo "$FILENAME:$LICENSE" >> metadata.txt
+	done )
+
+	touch conf/metadata.txt
+	cat conf/metadata.txt >> ${CACHEDIR}/metadata.txt
+	cat ${CACHEDIR}/metadata.txt | sort | uniq > conf/metadata.txt
+fi
