8b3bcda50155b761e8f65f57af6a311f195e05e4
Koen Kooi <koen@dominion.thruhere.net>
2010-11-22 16:04:35 +0100

    narcissus: change prune to clean work/ more aggressively than deploy/
    Signed-off-by: Koen Kooi <k-kooi@ti.com>

--- prune.php
+++ prune.php
@@ -5,6 +5,7 @@
  */
 
 passthru ("find /tmp -name 'opkg*' -mtime +2 -exec rm -r {} \;&& exit");
-passthru ("find deploy -depth -mindepth 1 -mtime +3 -delete && exit");
+passthru ("find deploy -depth -mindepth 2 -mtime +3 -exec rm -r {} \;&& exit");
+passthru ("find work -depth -mindepth 1 -maxdepth 2 -mtime +2 -exec rm -rf {} \;&& exit");
 
 ?>
