20705790b637cd4c34bcb44b6bf486f894395aa8
Koen Kooi <koen@dominion.thruhere.net>
2010-11-24 18:26:25 +0100

    narcissus: tweak prune script a bit
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- prune.php
+++ prune.php
@@ -1,11 +1,16 @@
+<pre>
 <?
 /* Narcissus - Online image builder for the angstrom distribution
  * Koen Kooi (c) 2010 - GPLv2
  *
  */
 
-passthru ("find /tmp -name 'opkg*' -mtime +2 -exec rm -r {} \;&& exit");
-passthru ("find deploy -depth -mindepth 2 -maxdepth 2 -mtime +3 -exec rm -r {} \;&& exit");
-passthru ("find work -depth -mindepth 1 -maxdepth 2 -mtime +2 -exec rm -rf {} \;&& exit");
+print "rm /tmp\n";
+passthru ("find /tmp -name 'opkg*' -mtime +2 -exec rm -rv {} \;&& exit");
+print "rm deploy\n";
+passthru ("find deploy -depth -mindepth 2 -maxdepth 2 -mtime +3 -exec rm -rv {} \;&& exit");
+print "rm work\n";
+passthru ("find work -depth -mindepth 1 -maxdepth 2 -mtime +1 -exec rm -rfv {} \;&& exit");
 
 ?>
+</pre>
