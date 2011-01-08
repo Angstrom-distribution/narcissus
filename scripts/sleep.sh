547cfd45f32aa8607e04966fa233125ef1610bac
Koen Kooi <koen@dominion.thruhere.net>
2011-01-08 10:08:31 +0100

    narcissus: fix typo in sleep script
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- scripts/sleep.sh
+++ scripts/sleep.sh
@@ -3,7 +3,7 @@
 LOAD="$(cat /proc/loadavg | awk -F. '{print $1}')"
 
 if [ $LOAD -gt 10 ] ; then
-	echo "load to high: $LOAD, sleeping"
+	echo "load too high: $LOAD, sleeping"
 	sleep 10
 else
 	echo "load acceptable: $LOAD, continuing"
