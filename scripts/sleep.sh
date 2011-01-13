75b9a876961345352d3159662d4d6c4d0b45df12
Koen Kooi <koen@dominion.thruhere.net>
2011-01-13 11:54:11 +0100

    narcissus: make sleep script linear with load
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- scripts/sleep.sh
+++ scripts/sleep.sh
@@ -2,9 +2,11 @@
 
 LOAD="$(cat /proc/loadavg | awk -F. '{print $1}')"
 
-if [ $LOAD -gt 10 ] ; then
+if [ $LOAD -gt 8 ] ; then
 	echo "load too high: $LOAD, sleeping"
-	sleep 10
+	sleep 20
+	sleep $LOAD
+	echo "$(date) load too high: $LOAD, sleeping" >> /tmp/foo
 else
 	echo "load acceptable: $LOAD, continuing"
 fi
