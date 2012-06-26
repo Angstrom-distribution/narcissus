0e99cd1afe8662c1d6ea079148d414e480c44b3d
Koen Kooi <koen@dominion.thruhere.net>
2012-06-26 11:34:14 +0200

    narcissus: include all items from ROOTFS_PKGMANAGE during base opkg install
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- scripts/configure-image.sh
+++ scripts/configure-image.sh
@@ -75,4 +75,5 @@ bin/opkg-cl ${CACHE} -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf -t ${OPKG_T
 echo "running: opkg-cl ${CACHE} -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf -t ${OPKG_TMP_DIR} upgrade"
 bin/opkg-cl ${CACHE} -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf -t ${OPKG_TMP_DIR} upgrade
 bin/opkg-cl ${CACHE} -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf -t ${OPKG_TMP_DIR} install opkg-config-base
+bin/opkg-cl ${CACHE} -o ${TARGET_DIR} -f ${TARGET_DIR}/etc/opkg.conf -t ${OPKG_TMP_DIR} install opkg opkg-collateral
 echo "Configure done"
